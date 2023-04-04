f70dc97b49921b01df9a6ea699bf65bd776be1d9
moxey.eth <jakemoxey@gmail.com>
2023-04-05 09:58:46 +1000

    fix: top-level await

--- index.tsx
+++ index.tsx
@@ -5,46 +5,49 @@ import { Address, createWalletClient, custom } from 'viem';
 import { mainnet } from 'viem/chains';
 import { EthereumProvider } from '@walletconnect/ethereum-provider';
 
-const provider = await EthereumProvider.init({
-  projectId: '2d8155e8754dd573bb58a45c3f4ec083',
-  chains: [mainnet.id],
-  showQrModal: true,
-});
+(async () => {
+  const provider = await EthereumProvider.init({
+    projectId: '2d8155e8754dd573bb58a45c3f4ec083',
+    chains: [mainnet.id],
+    showQrModal: true,
+  });
+  
+  const walletClient = createWalletClient({
+    chain: mainnet,
+    transport: custom(provider),
+  });
+  
+  function Example() {
+    const [account, setAccount] = useState<Address>();
+  
+    const connect = async () => {
+      await provider.connect();
+      const [address] = await walletClient.getAddresses();
+      setAccount(address);
+    };
+  
+    const sendTransaction = async () => {
+      if (!account) return;
+  
+      // Simulate a long-running async task.
+      await new Promise(res => setTimeout(() => res(''), 1000))
+  
+      await walletClient.sendTransaction({
+        account,
+        value: 0n
+      });
+    };
+  
+    if (account)
+      return (
+        <>
+          <div>Connected: {account}</div>
+          <button onClick={sendTransaction}>Send Transaction</button>
+        </>
+      );
+    return <button onClick={connect}>Connect Wallet</button>;
+  }
+  
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<Example />);
+})()
 
-const walletClient = createWalletClient({
-  chain: mainnet,
-  transport: custom(provider),
-});
-
-function Example() {
-  const [account, setAccount] = useState<Address>();
-
-  const connect = async () => {
-    await provider.connect();
-    const [address] = await walletClient.getAddresses();
-    setAccount(address);
-  };
-
-  const sendTransaction = async () => {
-    if (!account) return;
-
-    // Simulate a long-running async task.
-    await new Promise(res => setTimeout(() => res(''), 1000))
-
-    await walletClient.sendTransaction({
-      account,
-      value: 0n
-    });
-  };
-
-  if (account)
-    return (
-      <>
-        <div>Connected: {account}</div>
-        <button onClick={sendTransaction}>Send Transaction</button>
-      </>
-    );
-  return <button onClick={connect}>Connect Wallet</button>;
-}
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<Example />);
